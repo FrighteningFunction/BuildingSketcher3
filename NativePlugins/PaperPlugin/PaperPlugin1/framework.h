551ca85b36f00693799f136213834de4d5f158c7
Szokoly-Angyal Armand <szokoly-angyal.armand@outlook.com>
2025-05-11 17:10:30 +0200

    restart AndroidPlugin nightmare

--- NativePlugins/PaperPlugin/PaperPlugin1/framework.h
+++ NativePlugins/PaperPlugin/PaperPlugin1/framework.h
@@ -2,4 +2,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
-#include <windows.h>
+
+#ifdef _WIN32
+	#include <windows.h>
+#endif
